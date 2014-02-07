05e8b9f9d34571c73db45decd94ed755ab64758a
Chris Tucker <chris.tucker@service-now.com>
2014-02-07 10:14:10 -0800

    Better test error messages

--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -73,7 +73,7 @@ public class TennisTest {
             if (i < this.player2Score)
                 game.wonPoint("player2");
         }
-        assertEquals(this.expectedScore, game.getScore());
+        assertEquals(String.format("Score at %d-%d", this.player1Score, this.player2Score), this.expectedScore, game.getScore());
     }
 
     @Test
